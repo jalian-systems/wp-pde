99b338475ef1ac6171eed0e7ca1721e6f83fae7c
Dakshinamurthy Karra <dakshinamurthy.karra@jaliansystems.com>
2012-04-04 16:51:37 +0530

    Updated readme, php files to unix format. Changed README.md to readme.md in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -50,7 +50,7 @@ git checkout-index -a -f --prefix=$SVNPATH/trunk/
 
 echo "Ignoring github specific files and deployment script"
 svn propset svn:ignore "deploy.sh
-README.md
+readme.md
 .git
 .gitignore" "$SVNPATH/trunk/"
 
